c804c6f72570615b9b4a9e3d626b19b8428b8e3e
RamonPortuga <ramon.oliveira.azevedo@gmail.com>
2023-06-18 20:31:48 -0300

    (update) O Join, os getters, os setters e os ids de Threads foram adicionados

--- ParallelProgramming_Final_Work/src/Main.java
+++ ParallelProgramming_Final_Work/src/Main.java
@@ -9,15 +9,18 @@ class FileProcessingThread extends Thread {
     private String substring;
     private List<Integer> occurrenceLines;
 
-    public FileProcessingThread(String fileName, String substring) {
+    private int idThreads;
+
+    public FileProcessingThread(String fileName, String substring, int idThreads) {
         this.fileName = fileName;
         this.substring = substring;
+        this.idThreads = idThreads;
         this.occurrenceLines = new ArrayList<>();
     }
 
     @Override
     public void run() {
-        System.out.println("Thread " + Thread.currentThread().getId() + " processando arquivo: " + fileName);
+        System.out.println("Thread " + idThreads + " processando arquivo: " + fileName);
 
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
@@ -32,7 +35,7 @@ class FileProcessingThread extends Thread {
             System.out.println("Erro ao ler o arquivo: " + fileName);
         }
 
-        System.out.println("Thread " + Thread.currentThread().getId() + " finalizou o processamento do arquivo: " + fileName);
+        System.out.println("Thread " + idThreads + " finalizou o processamento do arquivo: " + fileName);
         if (!occurrenceLines.isEmpty()) {
             System.out.println("Ocorrências da substring '" + substring + "' no arquivo '" + fileName + "':");
             for (int line : occurrenceLines) {
@@ -42,16 +45,57 @@ class FileProcessingThread extends Thread {
             System.out.println("A substring '" + substring + "' não foi encontrada no arquivo '" + fileName + "'.");
         }
     }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getSubstring() {
+        return substring;
+    }
+
+    public void setSubstring(String substring) {
+        this.substring = substring;
+    }
+
+    public List<Integer> getOccurrenceLines() {
+        return occurrenceLines;
+    }
+
+    public void setOccurrenceLines(List<Integer> occurrenceLines) {
+        this.occurrenceLines = occurrenceLines;
+    }
+
+    public int getIdThreads() {
+        return idThreads;
+    }
+
+    public void setIdThreads(int idThreads) {
+        this.idThreads = idThreads;
+    }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        String[] fileNames = { "C:/Users/ramon/IdeaProjects/ParallelProgramming_Final_Work/src/arquivo1.txt", "C:/Users/ramon/IdeaProjects/ParallelProgramming_Final_Work/src/arquivo2.txt", "C:/Users/ramon/IdeaProjects/ParallelProgramming_Final_Work/src/arquivo3.txt" };
+    public static void main(String[] args) throws InterruptedException {
+        int sizeThreads = 3;
+
+        FileProcessingThread[] threads = new FileProcessingThread[sizeThreads];
+
+        //String path = "C:/Users/ramon/Documents/UFRJ/ProgConc/TrabalhoFinal/ParallelProgramming_Final_Work/src/Arquivo"
+        String[] fileNames = {"C:/Users/ramon/Documents/UFRJ/ProgConc/TrabalhoFinal/ParallelProgramming_Final_Work/src/Arquivos/arquivo1.txt", "C:/Users/ramon/Documents/UFRJ/ProgConc/TrabalhoFinal/ParallelProgramming_Final_Work/src/Arquivos/arquivo2.txt", "C:/Users/ramon/Documents/UFRJ/ProgConc/TrabalhoFinal/ParallelProgramming_Final_Work/src/Arquivos/arquivo3.txt"};
         String substring = "exemplo";
 
-        for (String fileName : fileNames) {
-            FileProcessingThread thread = new FileProcessingThread(fileName, substring);
-            thread.start();
+        for (int i = 0; i < sizeThreads; i++) {
+            threads[i] = new FileProcessingThread(fileNames[i], substring, i);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < sizeThreads; i++) {
+            threads[i].join();
         }
     }
 }
