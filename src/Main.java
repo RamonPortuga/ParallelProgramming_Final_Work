20f9956abafa15e6c156209e2613338ef0638453
GabrielTC9 <gabriel.trindadecorrea@gmail.com>
2023-06-27 00:25:40 -0300

    (update) usuário agora insere o número de threads por arquivo, programa não para se 1 path estiver errado, ajuste da saída em andamento

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,36 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class FileProcessingThread extends Thread {
     private String fileName;
     private String substring;
-    private List<Integer> occurrenceLines;
     private int idThreads;
     private File file;
     private final String fileExtension;
-
+    private List<Integer> occurrenceLines;
     private int occurrenceCounter = 0;
+    private final List<String> lines;
+    private final int threadPerFile;
 
-    public FileProcessingThread(String fileName, String substring, int idThreads, File file, String fileExtension) {
+    public FileProcessingThread(String fileName, String substring, int idThreads, File file, String fileExtension, int threadPerFile) {
         this.fileName = fileName;
         this.substring = substring;
         this.idThreads = idThreads;
         this.file = file;
-        this.occurrenceLines = new ArrayList<>();
         this.fileExtension = fileExtension;
+        this.threadPerFile = threadPerFile;
+        this.occurrenceLines = new ArrayList<>();
+        this.lines = new ArrayList<>();
     }
 
-    @Override
-    public void run() {
-
+    public void preProcessing(){
         if (fileExtension.equals("txt")) {
 
             try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
                 String line;
-                int lineNumber = 1;
 
                 while ((line = reader.readLine()) != null) {
-                    int index = line.toLowerCase().indexOf(substring.toLowerCase());
-                    while (index != -1) {
-                        if(occurrenceLines.isEmpty()){
-                            occurrenceLines.add(lineNumber);
-                        }
-                        else if (occurrenceLines.get(occurrenceLines.size() - 1) != lineNumber){
-                            occurrenceLines.add(lineNumber);
-                        }
-                        occurrenceCounter++;
-                        index = line.toLowerCase().indexOf(substring.toLowerCase(), index + 1);
-                    }
-                    lineNumber++;
+                    lines.add(line);
                 }
             } catch (IOException e) {
                 System.out.println("Erro ao ler o arquivo: " + fileName);
@@ -57,39 +43,39 @@ class FileProcessingThread extends Thread {
                 DataInputStream input = new DataInputStream (inputStream);
 
                 String[] fileContentByLines = input.readUTF().split("\n");
-                int lineNumber = 1;
-                for (String fileContentByLine : fileContentByLines) {
-                    int index = fileContentByLine.toLowerCase().indexOf(substring.toLowerCase());
-                    while (index != -1) {
-                        if(occurrenceLines.isEmpty()){
-                            occurrenceLines.add(lineNumber);
-                        }
-                        else if (occurrenceLines.get(occurrenceLines.size() - 1) != lineNumber){
-                            occurrenceLines.add(lineNumber);
-                        }
-                        occurrenceCounter++; // Incrementa o contador de ocorrências
-                        index = fileContentByLine.toLowerCase().indexOf(substring.toLowerCase(), index + 1);
-                    }
-                    lineNumber++;
-                }
+                Collections.addAll(lines, fileContentByLines);
 
                 inputStream.close();
                 input.close();
-
             } catch (FileNotFoundException e) {
                 System.out.println("Erro na procura do arquivo: " + fileName);
             } catch (IOException e) {
                 System.out.println("Erro ao ler o arquivo: " + fileName);
             }
         }
+    }
 
+    @Override
+    public void run() {
+        int lineNumber = 1;
+        for (int i = idThreads%threadPerFile; i < lines.size(); i += threadPerFile) {
+            int index = lines.get(i).toLowerCase().indexOf(substring.toLowerCase());
+            while (index != -1) {
+                if (!occurrenceLines.contains(lineNumber)) {
+                    occurrenceLines.add(lineNumber);
+                }
+                occurrenceCounter++;
+                index = lines.get(i).toLowerCase().indexOf(substring.toLowerCase(), index + 1);
+            }
+            lineNumber = i + 1; // Incrementar o número da linha
+        }
     }
 
     public synchronized void checkOccurrences(){
         int separatorIndex = fileName.lastIndexOf("/");
-        System.out.println("Thread de ID " + idThreads + " executou arquivo " + fileName.substring(separatorIndex + 1));
+        //System.out.println("Thread de ID " + idThreads + " executou arquivo " + fileName.substring(separatorIndex + 1));
 
-        System.out.println("O arquivo possui " + file.length() + " bytes");
+        System.out.println("O arquivo ["+fileName.substring(separatorIndex + 1)+"] possui " + file.length() + " bytes");
 
         if (!occurrenceLines.isEmpty()) {
             StringBuilder sb = new StringBuilder();
@@ -201,103 +187,95 @@ class WriteBinaryFile {
 
 public class Main {
     public static void main(String[] args) throws InterruptedException{
-
-        /*
-        String[] fileNames = {
-                "C:/Users/Vitória Nazareth/Desktop/COMP_CON/ParallelProgramming_Final_Work/src/Arquivos/arquivo1.txt",
-                "C:/Users/Vitória Nazareth/Desktop/COMP_CON/ParallelProgramming_Final_Work/src/Arquivos/arquivo2.txt",
-                "C:/Users/Vitória Nazareth/Desktop/COMP_CON/ParallelProgramming_Final_Work/src/Arquivos/arquivo3.txt",
-                "C:/Users/Vitória Nazareth/Desktop/COMP_CON/ParallelProgramming_Final_Work/src/Arquivos/arquivo4.txt"
-        };
-        */
-        //String path = "C:/Users/ramon/Documents/UFRJ/ProgConc/TrabalhoFinal/ParallelProgramming_Final_Work/src/Arquivo"
-        //String path = "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos"
-
-        //PARA ARQUIVOS TXT
-        /*
-        String[] fileNames = {
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/arquivo1.txt",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/arquivo2.txt",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/arquivo3.txt",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/arquivo4.txt"
-        };
-        */
-
-        //PARA ARQUIVOS BIN
-        /*
-        String[] fileNames = {
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/teste1.bin",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/teste2.bin",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/teste3.bin",
-                "C:/Users/ramon/Documents/UFRJ/ProgConc/ParallelProgramming_Final_Work/src/Arquivos/teste4.bin"
-        };
-         */
-
-        /*
-        String[] fileNames = {
-                "src/Arquivos/arquivo1.txt",
-                "src/Arquivos/arquivo2.txt",
-                "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos/arquivo3.txt",
-                "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos/arquivo4.txt"
-        };
-        */
-        /*
-        String[] fileNames = {
-                "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos/teste1.bin",
-                "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos/teste2.bin",
-                "C:/Users/gabri/IdeaProjects/ParallelProgramming_Final_Work/src/Arquivos/teste3.bin"
-        };
-        */
-
         Scanner input = new Scanner(System.in);
+
+        //User insert necessary values: substring to search, number of files, thread per file, paths
         System.out.println("Digite a palavra a ser procurada:");
         String substring = input.nextLine();
 
         System.out.println("Digite o número de arquivos a serem processados:");
-        boolean rightType = false;
-        int sizeThreads = 0;
-        while(!rightType){
+        int numberOfFiles;
+        while(true){
             try {
-                sizeThreads = input.nextInt();
-                rightType = true;
+                numberOfFiles = input.nextInt();
+                break;
             } catch (InputMismatchException e){
                 System.out.println("Por favor, digite um número inteiro:");
                 input.next();
             }
         }
-        FileProcessingThread[] threads = new FileProcessingThread[sizeThreads];
 
-        String[] fileNames = new String[sizeThreads];
+        System.out.println("Digite o número de threads para cada arquivo:");
+        int threadPerFile;
+        while(true){
+            try {
+                threadPerFile = input.nextInt();
+                break;
+            } catch (InputMismatchException e){
+                System.out.println("Por favor, digite um número inteiro:");
+                input.next();
+            }
+        }
+        int totalThreadNumber = numberOfFiles * threadPerFile;
+        FileProcessingThread[] threads = new FileProcessingThread[totalThreadNumber];
+
         System.out.println("Digite o path completo para o arquivo a ser processado, ou o path do arquivo na pasta Arquivos (src/Arquivos/nomedoarquivo.extensao):");
-        for(int i=0; i<sizeThreads; i++){
+        String[] fileNames = new String[numberOfFiles];
+        for(int i=0; i<numberOfFiles; i++){
             System.out.print("Arquivos "+(i+1)+": ");
             fileNames[i] = input.next();
         }
         System.out.println();
+
         input.close();
 
+        //Get current time in milliseconds to check at the end
         long startTime = System.currentTimeMillis();
 
-        for (int i = 0; i < sizeThreads; i++) {
-            String extension = fileNames[i].substring(fileNames[i].lastIndexOf(".")+1);
+        //Create file processing threads with extension check
+        for (int i = 0; i < totalThreadNumber; i++) {
+
+            int fileNameIndex = i/threadPerFile;
+            //System.out.println(fileNameIndex);
+            String extension = fileNames[fileNameIndex].substring(fileNames[fileNameIndex].lastIndexOf(".")+1);
 
             if(extension.equals("txt") || extension.equals("bin")) {
-                File file = new File(fileNames[i]);
-                threads[i] = new FileProcessingThread(fileNames[i], substring, i, file,  extension);
-                threads[i].start();
+                File file = new File(fileNames[fileNameIndex]);
+                threads[i] = new FileProcessingThread(fileNames[fileNameIndex], substring, i, file,  extension, threadPerFile);
             }
             else {
-                System.out.println("Arquivo do tipo ["+extension+"] sem suporte. Utilize [txt] ou [bin]");
-                System.exit(-1);
+                System.out.println("Erro na Thread ID "+i+": Arquivo ["
+                                    +fileNames[fileNameIndex].substring(fileNames[fileNameIndex].lastIndexOf("/")+1)+
+                                    "] do tipo ["+extension+"] sem suporte. Utilize [txt] ou [bin]\n");
+                threads[i] = null;
             }
         }
 
-        for (int i = 0; i < sizeThreads; i++) {
+        for (int i = 0; i < totalThreadNumber; i++) {
+            if(threads[i]==null) continue;
+            threads[i].preProcessing();
+        }
+
+        for (int i = 0; i < totalThreadNumber; i++) {
+            if(threads[i]==null) continue;
+            threads[i].start();
+        }
+
+        for (int i = 0; i < totalThreadNumber; i++) {
+            if(threads[i]==null) continue;
             threads[i].join();
         }
 
-        for (int i = 0; i < sizeThreads; i++) {
-            threads[i].checkOccurrences();
+        int currentFile = -1;
+        for (int i = 0; i < totalThreadNumber; i ++) {
+            if(threads[i]==null) continue;
+            //threads[i].checkOccurrences();
+            ///*
+            if(threads[i].getIdThreads()/threadPerFile != currentFile){
+                currentFile = threads[i].getIdThreads();
+                threads[i].checkOccurrences();
+            //*/
+            }
         }
 
         long endTime = System.currentTimeMillis();
